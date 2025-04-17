bba493df545a4c0dcec578a3682d3f2df5a84838
Rohan Prakash <pksh.rohan16@gmail.com>
2025-04-17 12:07:50 +0530

    Initial Commit

---
+++ JansunwAI/JansunwAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JansunwAIApp.swift
+//  JansunwAI
+//
+//  Created by Rohan Prakash on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct JansunwAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
